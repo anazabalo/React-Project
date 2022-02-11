ab30845c8bd9b585058d82a882d511bd13cb28ff
Ana <zabaloana@gmail.com>
2022-02-11 16:59:17 +0100

    Adding first product

--- project-ecommerce/src/components/Products/index.js
+++ project-ecommerce/src/components/Products/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import IMG from "../../images/img01.jpg"
 
 export const ProductList = () => {
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <>
+      <h1 className="title"> PRODUCTS</h1>
+      <div className="products">
+        <div className="product">
+          <a href="#">
+          <div className="product-img">
+            <img src={IMG} alt=""/>
+          </div> 
+          </a>
+          <div className="product-footer">
+            <h3> Title</h3>
+            <p> Category</p>
+            <p className="price">$320</p>
+        </div>
+        <div className="buttons">
+          <button className="btn"> Add to Cart</button>
+        </div>
+        <div>
+          <a href="#" className="btn"> View </a>
+        </div>
+        </div>
+      </div>    
+    </>
+    
   );
 };
